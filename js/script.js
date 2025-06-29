1542cccbd1320f471fc15e7962da8f7a71759c8e
Felipe Tozadori Silva <ftozadori@gmail.com>
2025-06-29 14:52:52 -0300

    Land Page de Previsão do Tempo

---
+++ js/script.js
@@ -0,0 +1,57 @@
+document.querySelector('#search').addEventListener('submit',async (event) => {
+    event.preventDefault();
+
+    const cityName = document.querySelector('#city_name').value;
+
+    if(!cityName){
+        document.querySelector('#weather').classList.remove('show');
+        showAlert('Você precisa digitar uma cidade...');
+        return;
+    }
+
+    const apiKey = '11fb972209b199a8face968dce121ba3';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(city_name)}&appid=${apiKey}&units=metric&lang=pt_br`
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+
+    if(json.cod === 200){
+        showInfo({
+            city: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.min,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
+            windSpeed: json.wind.speed,
+            humidity: json.main.humidity,
+        });
+    } else {
+        document.querySelector('#weather').classList.remove('show');
+        showAlert(`
+            Não foi possível localizar...
+
+            <img src="img/undraw_taken_mshk.svg"/>
+        `)
+    }
+});
+
+function showInfo(json){
+    showAlert('');
+
+    document.querySelector('#weather').classList.add('show')
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_description').innerHTML = `${json.description}`;
+    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('#tem_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#tem_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#humidity').innerHTML = `${json.humidity}`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
+}
+
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
+}
